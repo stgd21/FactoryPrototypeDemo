1ef848677793878f4dcb67c05b1c4012fbb66482
stgd21 <45217325+stgd21@users.noreply.github.com>
2020-01-21 17:17:17 -0700

    Add files via upload

---
+++ Scripts/Prototype/Client.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Client : MonoBehaviour
+{
+    public Drone m_Drone;
+    public Sniper m_Sniper;
+    public Gamer m_Gamer;
+    public EnemySpawner m_Spawner;
+
+    private Enemy m_Spawn;
+    private int m_IncrementorDrone = 0;
+    private int m_IncrementorSniper = 0;
+    private int m_IncrementGamer = 0;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            m_Spawn = m_Spawner.SpawnEnemy(m_Drone);
+
+            m_Spawn.name = "Drone_Clone_" + ++m_IncrementorDrone;
+            m_Spawn.transform.Translate(Vector3.forward * m_IncrementorDrone * 1.5f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            m_Spawn = m_Spawner.SpawnEnemy(m_Sniper);
+
+            m_Spawn.name = "Sniper_Clone_" + ++m_IncrementorSniper;
+            m_Spawn.transform.Translate(Vector3.forward * m_IncrementorSniper * 1.5f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            m_Spawn = m_Spawner.SpawnEnemy(m_Gamer);
+
+            m_Spawn.name = "Gamer_Clone_" + ++m_IncrementGamer;
+            m_Spawn.transform.Translate(Vector3.forward * m_IncrementGamer * 1.5f);
+        }
+    }
+}
